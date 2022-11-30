119742ccdc570786ffe616d8b20d4f3ebe05b5c6
Konstantin Belin <kbel01@mail.ru>
2022-11-30 22:02:14 +0700

    MeetAStranger and HelloAutocode

---
+++ com.epam.training.student_Konstantin_Belin/fundamentals/main_task/HelloAutocode.java
@@ -0,0 +1,8 @@
+package com.epam.training.student_Konstantin_Belin.fundamentals.main_task;
+
+public class HelloAutocode {
+    public static void main (String [] args){
+
+        System.out.println("Hello, Autocode!");
+    }
+}
---
+++ com.epam.training.student_Konstantin_Belin/fundamentals/main_task/MeetAStranger.java
@@ -0,0 +1,14 @@
+package com.epam.training.student_Konstantin_Belin.fundamentals.main_task;
+
+import java.util.Scanner;
+
+public class MeetAStranger {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        System.out.printf("type some text ");
+        String txt = in.nextLine();
+        System.out.printf("Hello, %s ", txt);
+        in.close();
+    }
+}
